df551771bf013787590637b09ccbf86401cefc88
sharukhhasan <sharukh-hasan@uiowa.edu>
2016-11-29 10:42:06 -0600

    implemented login serializable class for packets

--- CryptoMessenger/src/main/java/packets/Login.java
+++ CryptoMessenger/src/main/java/packets/Login.java
@@ -1,7 +1,17 @@
 package packets;
 
+import java.io.Serializable;
+
 /**
  * Created by Sharukh Hasan on 11/29/16.
  */
-public class Login {
+public class Login implements Serializable {
+    private static final long serialVersionUID = -5849519141227484158L;
+
+    public String username, passwordHash;
+
+    public Login(String username, String passwordHash){
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
 }
