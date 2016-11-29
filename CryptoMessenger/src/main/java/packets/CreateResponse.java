d68810052a83912fc13d20d4968120767a68b1e7
sharukhhasan <sharukh-hasan@uiowa.edu>
2016-11-29 10:45:32 -0600

    response for create packets

--- CryptoMessenger/src/main/java/packets/CreateResponse.java
+++ CryptoMessenger/src/main/java/packets/CreateResponse.java
@@ -1,7 +1,16 @@
 package packets;
 
+import java.io.Serializable;
+
 /**
  * Created by Sharukh Hasan on 11/29/16.
  */
-public class CreateResponse {
+public class CreateResponse implements Serializable {
+    private static final long serialVersionUID = 1525631574743843821L;
+
+    public boolean creationSuccessful;
+
+    public CreateResponse(boolean success) {
+        this.creationSuccessful = success;
+    }
 }
