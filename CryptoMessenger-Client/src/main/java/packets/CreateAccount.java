ef1065b1e7c975546890cd209a1f800e6aa1a01b
sharukhhasan <sharukh-hasan@uiowa.edu>
2016-11-29 12:08:44 -0600

    account creation - data packet

--- CryptoMessenger-Client/src/main/java/packets/CreateAccount.java
+++ CryptoMessenger-Client/src/main/java/packets/CreateAccount.java
@@ -1,7 +1,17 @@
 package packets;
 
+import java.io.Serializable;
+
 /**
- * Created by sharukhhasan on 11/29/16.
+ * Created by Sharukh Hasan on 11/29/16.
  */
-public class CreateAccount {
+public class CreateAccount implements Serializable {
+    private static final long serialVersionUID = 6200495373556789975L;
+
+    public String username, passwordHash;
+
+    public CreateAccount(String username, String passwordHash){
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
 }
