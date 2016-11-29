0f36194b42f138e86c2da58b11487ef5f609a7cb
sharukhhasan <sharukh-hasan@uiowa.edu>
2016-11-29 10:42:27 -0600

    implemented login serializable class for response packets

--- CryptoMessenger/src/main/java/packets/LoginResponse.java
+++ CryptoMessenger/src/main/java/packets/LoginResponse.java
@@ -1,7 +1,16 @@
 package packets;
 
+import java.io.Serializable;
+
 /**
  * Created by Sharukh Hasan on 11/29/16.
  */
-public class LoginResponse {
+public class LoginResponse implements Serializable {
+    private static final long serialVersionUID = 724043919519375872L;
+
+    public boolean loginSuccessful;
+
+    public LoginResponse(boolean success) {
+        this.loginSuccessful = success;
+    }
 }
