a254d740e903e385d6ec39252b3d1b43fa65ff84
sharukhhasan <sharukh-hasan@uiowa.edu>
2016-11-29 12:09:48 -0600

    ending thread connection to server

--- CryptoMessenger-Client/src/main/java/packets/CloseConnection.java
+++ CryptoMessenger-Client/src/main/java/packets/CloseConnection.java
@@ -1,7 +1,12 @@
 package packets;
 
+import java.io.Serializable;
+
 /**
- * Created by sharukhhasan on 11/29/16.
+ * Created by Sharukh Hasan on 11/29/16.
  */
-public class CloseConnection {
+public class CloseConnection implements Serializable {
+    private static final long serialVersionUID = 6655568423356180371L;
+
+    public CloseConnection() {}
 }
